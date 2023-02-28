c627eff1e730f0d8a0f3a972af30ceffe2d98922
deepakb1992 <deepak.bh@outlook.com>
2023-02-28 13:24:18 +0530

    with conditions

--- server.js
+++ server.js
@@ -1,23 +1,57 @@
 const express = require("express");
-const multer = require("multer");
-
 const app = express();
+const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
-//
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const bcrypt = require("bcrypt");
 
-app.use(express.static("public"));
+// Fake user database
+const users = [];
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
+// Middleware to log incoming requests
+app.use(function(req, res, next) {
+  console.log(req.method, req.url, req.body);
+  next();
 });
 
+// Route to handle user signups
 app.post("/signup", upload.single("image"), function(req, res) {
-  console.log(req.body);
+  // Validate the user input
+  if (!req.body.name || !req.body.email || !req.body.password || !req.body.age) {
+    return res.status(400).send("Missing required fields");
+  }
 
+  // Check if the email already exists in the database
+  const existingUser = users.find(user => user.email === req.body.email);
+  if (existingUser) {
+    return res.status(409).send("Email already exists");
+  }
+
+  // Hash the password and store the user data in the database
+  bcrypt.hash(req.body.password, 10, function(err, hash) {
+    if (err) {
+      return res.status(500).send("Internal server error");
+    }
+    const newUser = {
+      id: generateUniqueId(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hash,
+      age: req.body.age,
+      image: req.file ? req.file.filename : null
+    };
+    users.push(newUser);
+
+    // Return a response indicating success
+    return res.status(201).send("User created");
+  });
 });
 
+// Helper function to generate a unique ID for a new user
+function generateUniqueId() {
+  return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+}
+
+// Start the server
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
