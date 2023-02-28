d163246813fbccbd89c9a4a3f10dcf799dc1f50a
Deepak <deepak.bh@outlook.com>
2023-02-28 12:51:52 +0530

    changes to server.js

--- server.js
+++ server.js
@@ -14,12 +14,8 @@ app.get("/", function(req, res) {
 });
 
 app.post("/signup", upload.single("image"), function(req, res) {
-  // Log the request body to the console for debugging
   console.log(req.body);
 
-  // TODO: Store the user data in a database or file
-  // TODO: Generate a unique ID for the user and store it with the user data
-  // TODO: Return a response indicating success or failure
 });
 
 app.listen(3000, function() {
